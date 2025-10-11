6388ec2b1578fb1fca7c3c2219e1b9f7856bbdb5
yasintuncerr <yasintuncerr@gmail.com>
2025-10-11 16:01:40 +0300

    Add still source header file with structure definitions and function prototypes

---
+++ include/http/still-source.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later
+ * Abstract still source
+ * Copyright (C) 2025 Yasin Tunçer
+ *
+ * Contact: Yasin Tunçer <yasintuncerr@gmail.com>
+ */
+
+#ifndef __STILL_SOURCE_H__
+#define __STILL_SOURCE_H__
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/time.h>
+
+
+struct v4l2_pix_format;
+struct still_source;
+struct still_buffer;
+
+struct still_source_ops {
+    void(*destroy)(struct still_source *src);
+    int(*set_format)(struct still_source *src, struct v4l2_pix_format *fmt);
+    int(*alloc_buffer)(struct still_source *src);
+    int(*free_buffer)(struct still_source *src);
+    int(*capture)(struct still_source *src);
+    struct still_buffer *(*get_buffer)(struct still_source *src);
+};
+
+struct still_buffer {
+    unsigned int size;
+    unsigned int bytesused;
+    struct timeval timestamp;
+    bool error;
+    void *mem;
+};
+
+
+struct still_source {
+    const struct still_source_ops *ops;
+    
+};
+
+
+void still_source_destroy(struct still_source *src);
+int still_source_set_format(struct still_source *src, struct v4l2_pix_format *fmt);
+int still_source_alloc_buffer(struct still_source *src);
+int still_source_free_buffer(struct still_source *src);
+int still_source_capture(struct still_source *src);
+struct still_buffer *still_source_get_buffer(struct still_source *src);
+
+#endif /* __STILL_SOURCE_H__ */
