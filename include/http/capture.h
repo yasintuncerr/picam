72518391928296541e35aec00f24b37fb8e2a7c5
yasintuncerr <yasintuncerr@gmail.com>
2025-10-12 22:11:55 +0300

    Refactor HTTP capture header to improve clarity and structure

--- include/http/capture.h
+++ include/http/capture.h
@@ -7,22 +7,30 @@
  * Contact: Yasin Tunçer <yasintuncerr@gmail.com>
 */
 
-#ifndef __CAPTURE_H__
-#define __CAPTURE_H__
+#ifndef __HTTP_CAPTURE_H__
+#define __HTTP_CAPTURE_H__
 
-struct still_source;
-struct v4l2_pix_format;
+struct video_source;
+struct events;
 struct http_server;
 
 /**
- * http_capture_new - Create a new HTTP capture
+ * http_capture_new - Create a new HTTP capture server
  * @param port: Port number to run the HTTP server on
+ * @param vid_src: Video source instance (can be NULL)
+ * @param events: Event loop instance
  * 
  * Create a new HTTP capture instance that starts an HTTP server on the specified port.
  * The server will handle incoming requests for capturing images.
  * Returns a pointer to the newly created HTTP capture instance, or NULL on failure.
  */
-struct http_server *http_capture_new(int port);
-void http_capture_destroy(struct http_server *server);
-#endif /* __CAPTURE_H__ */
+struct http_server *http_capture_new(int port, struct video_source *vid_src, 
+                                     struct events *events);
 
+/**
+ * http_capture_destroy - Destroy HTTP capture server
+ * @param server: HTTP server instance to destroy
+ */
+void http_capture_destroy(struct http_server *server);
+
+#endif /* __HTTP_CAPTURE_H__ */
