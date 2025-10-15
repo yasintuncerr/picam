d2287d0fea95e822475165501a25388728cd9061
yasintuncerr <yasintuncerr@gmail.com>
2025-10-16 01:29:44 +0300

    Update still configuration pixel format to SBGGR12 for improved image quality

--- lib/libcamera-source.cpp
+++ lib/libcamera-source.cpp
@@ -411,7 +411,7 @@ static int libcamera_source_video_set_format(struct video_source *s,
 
 	if (src->config) {
 		StreamConfiguration &stillConfig = src->config->at(1);
-		stillConfig.pixelFormat = PixelFormat::fromString("BGR888");
+		stillConfig.pixelFormat = PixelFormat::fromString("SBGGR12");
 		stillConfig.size.width = 4056;
 		stillConfig.size.height = 3040;
 		stillConfig.bufferCount = 1;
