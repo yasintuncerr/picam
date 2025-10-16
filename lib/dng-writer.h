72ae93531b974106eaaaef72795c0b52948ad08d
yasintuncerr <yasintuncerr@gmail.com>
2025-10-16 23:02:07 +0300

    Add callback and buffer writing support to DNGWriter

--- lib/dng-writer.h
+++ lib/dng-writer.h
@@ -1,26 +1,77 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 /*
  * Copyright (C) 2020, Raspberry Pi Ltd
- *
  * DNG writer
+ * * Copyright (C) 2025 Yasin Tunçer
+ * * Added Callback support for DNG writing
+ * Added Write to Buffer support for DNG writing
  */
 
 #pragma once
 
 #ifdef HAVE_TIFF
 
+#include <functional>
+#include <memory> // std::shared_ptr için eklendi
+#include <vector>
+
 #include <libcamera/camera.h>
 #include <libcamera/controls.h>
 #include <libcamera/framebuffer.h>
 #include <libcamera/stream.h>
 
+extern "C" {
+#include "still-source.h"
+}
+
+
+struct StillBufferDeleter {
+    void operator()(still_buffer* b) const {
+        if (b) {
+            if (b->mem) {
+                free(b->mem);
+            }
+            delete b;
+        }
+    }
+};
+
+using DngBufferPtr = std::shared_ptr<still_buffer>;
+using DngReadyCallback = std::function<void(DngBufferPtr buffer)>;
+
 class DNGWriter
 {
 public:
-	static int write(const char *filename, const libcamera::Camera *camera,
-			 const libcamera::StreamConfiguration &config,
-			 const libcamera::ControlList &metadata,
-			 const libcamera::FrameBuffer *buffer, const void *data);
+    DNGWriter() = default;
+    ~DNGWriter() = default;
+    
+    // Kopyalama ve taşıma operasyonlarını engellemek iyi bir pratik olabilir
+    DNGWriter(const DNGWriter&) = delete;
+    DNGWriter& operator=(const DNGWriter&) = delete;
+    
+    void setCallback(DngReadyCallback callback) { dngReadyCallback_ = std::move(callback); }
+    void clearCallback() { dngReadyCallback_ = nullptr; }
+    
+    bool writeToBuffer(const libcamera::Camera *camera,
+                       const libcamera::StreamConfiguration &config,
+                       const libcamera::ControlList &metadata,
+                       const libcamera::FrameBuffer *buffer, const void *data);
+
+    static int write(const char *filename, const libcamera::Camera *camera,
+                     const libcamera::StreamConfiguration &config,
+                     const libcamera::ControlList &metadata,
+                     const libcamera::FrameBuffer *buffer, const void *data);
+
+private:
+    DngReadyCallback dngReadyCallback_;
+    
+    // Bu artık static değil, çünkü DNGWriter'ın bir üyesi olan callback'e erişmesi gerekiyor.
+    // Eğer callback static bir değişkende tutulsaydı static kalabilirdi.
+    // Alternatif olarak, callback'i parametre olarak da alabilirdi.
+    int writeInternal(TIFF *tif, const libcamera::Camera *camera,
+                      const libcamera::StreamConfiguration &config,
+                      const libcamera::ControlList &metadata,
+                      const void *data);
 };
 
 #endif /* HAVE_TIFF */
