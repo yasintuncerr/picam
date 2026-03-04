b51a9fa17a194e6b3e56737bfd07534a83fb80b1
yasintuncer <yasintuncerr@outlook.com>
2026-03-04 03:10:36 +0300

    fix: Explicitly convert camera model to `std::string` before passing its C-string to `TIFFSetField`.

--- lib/dng-writer.cpp
+++ lib/dng-writer.cpp
@@ -668,7 +668,7 @@ int DNGWriter::writeInternal( TIFF *tif, const Camera *camera,
 
 	const auto &model = cameraProperties.get(properties::Model);
 	if (model) {
-		TIFFSetField(tif, TIFFTAG_MODEL, model->c_str());
+		TIFFSetField(tif, TIFFTAG_MODEL, std::string(*model).c_str());
 		/* \todo set TIFFTAG_UNIQUECAMERAMODEL. */
 	}
 
